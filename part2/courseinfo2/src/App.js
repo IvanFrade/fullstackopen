3242561b35f7cbda1572c47c34d853cb4ea5589f
Ivan Fradellin <fradellin.ivan@gmail.com>
2021-11-28 14:32:39 +0100

    Exercise 2.5
    Course information, part 10

--- part2/courseinfo2/src/App.js
+++ part2/courseinfo2/src/App.js
@@ -1,40 +1,5 @@
 import React from 'react'
-
-const Courses = ({ courses }) => (
-  <div>
-    <h1>Web development curriculum</h1>
-    {courses.map(course => <Course key={course.id} course={course} />)}
-  </div>
-)
-
-const Course = ({ course }) => (
-  <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-  </div>
-)
-
-const Header = ({ course }) => <h2>{course.name}</h2>
-
-const Total = ({ course }) => {
-  const sum = course.parts.map(part => part.exercises).reduce((a, b) => a + b, 0)
-  return(
-    <p>Total of {sum} exercises</p>
-  ) 
-}
-
-const Content = ({ course }) => (
-  <div>
-    {course.parts.map(part => <Part key={part.id} part={part} />)}
-  </div>
-)
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>    
-)
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
@@ -82,7 +47,12 @@ const App = () => {
     }
   ]
 
-  return <Courses courses={courses} />
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map(course => <Course key={course.id} course={course} />)}
+    </div>
+  )
 }
 
 export default App;
